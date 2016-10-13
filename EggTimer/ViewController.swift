7676084a4896bee3c40dbf4f24c2ddf88d24d3a5
David <dmsmith@Davids-MacBook-Pro.local>
2016-10-13 18:36:23 -0400

    Working with Menu Bars/Toolbars

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timerLabel: UILabel!
+    
+    var time = 210
+    var timer = Timer()
+    
+    @IBAction func playButton(_ sender: AnyObject) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decrement), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseButton(_ sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func minus10Button(_ sender: AnyObject) {
+        if (time > 10) {
+            time -= 10
+            setTimerLabel()
+        }
+    }
+    
+    @IBAction func plus10Button(_ sender: AnyObject) {
+        time += 10
+        setTimerLabel()
+    }
+    
+    
+    @IBAction func resetButton(_ sender: AnyObject) {
+        time = 210
+        setTimerLabel()
+    }
+    
+    func decrement() {
+        if (time > 0) {
+            time -= 1
+            setTimerLabel()
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+    func setTimerLabel() {
+        timerLabel.text = String(time)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +63,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
